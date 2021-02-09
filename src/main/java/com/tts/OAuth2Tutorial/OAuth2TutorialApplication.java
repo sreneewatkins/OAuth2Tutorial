31082d99b8e2deecab0169b151143f1e36bf5f4e
sreneewatkins <74524591+sreneewatkins@users.noreply.github.com>
2021-02-09 15:39:03 -0600

    Added logout button and endpoint. Client side changes

--- src/main/java/com/tts/OAuth2Tutorial/OAuth2TutorialApplication.java
+++ src/main/java/com/tts/OAuth2Tutorial/OAuth2TutorialApplication.java
@@ -8,6 +8,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -34,6 +35,12 @@ public class OAuth2TutorialApplication extends WebSecurityConfigurerAdapter {
 				)
 				.exceptionHandling(e -> e
 						.authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+				)
+				.logout(l -> l
+						.logoutSuccessUrl("/").permitAll()
+				)
+				.csrf(c -> c
+						.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
 				)
 				.oauth2Login();
 		// @formatter:on
