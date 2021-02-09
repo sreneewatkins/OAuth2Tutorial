3626d62f9edf6fdb88c8ee9d1e280cc123062741
sreneewatkins <74524591+sreneewatkins@users.noreply.github.com>
2021-02-09 14:11:52 -0600

    Initial Commit

---
+++ src/main/java/com/tts/OAuth2Tutorial/OAuth2TutorialApplication.java
@@ -0,0 +1,14 @@
+package com.tts.OAuth2Tutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OAuth2TutorialApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(OAuth2TutorialApplication.class, args);
+	}
+
+}//end OAuth2TutorialApplication class
---
+++ src/test/java/com/tts/OAuth2Tutorial/OAuth2TutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tts.OAuth2Tutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OAuth2TutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
