de19c64db0f5c5d0b3255720af4588a337314c0b
sreneewatkins <74524591+sreneewatkins@users.noreply.github.com>
2021-02-09 19:58:08 -0600

    Added error msg and authentication failure in the client

--- src/main/java/com/tts/OAuth2Tutorial/OAuth2TutorialApplication.java
+++ src/main/java/com/tts/OAuth2Tutorial/OAuth2TutorialApplication.java
@@ -7,11 +7,16 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.security.web.authentication.AuthenticationEntryPointFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.net.ssl.HandshakeCompletedEvent;
+import javax.net.ssl.SSLSessionBindingEvent;
+import javax.servlet.http.HttpServletRequest;
 import java.util.Collections;
 import java.util.Map;
 
@@ -23,6 +28,14 @@ public class OAuth2TutorialApplication extends WebSecurityConfigurerAdapter {
 	public Map<String, Object> user(@AuthenticationPrincipal OAuth2User principal) {
 
 		return Collections.singletonMap("name", principal.getAttribute("name"));
+	}
+
+	@GetMapping("/error")
+	public String error() {
+		HttpServletRequest request = null;
+		String message = (String) request.getSession().getAttribute("error.message");
+		request.getSession().removeAttribute("error.message");
+		return message;
 	}
 
 	@Override
@@ -42,7 +55,14 @@ public class OAuth2TutorialApplication extends WebSecurityConfigurerAdapter {
 				.csrf(c -> c
 						.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
 				)
-				.oauth2Login();
+//				.oauth2Login();
+				.oauth2Login(o -> o
+						.failureHandler((request, response, exception) -> {
+							request.getSession().setAttribute("error.message", exception.getMessage());
+							AuthenticationFailureHandler handler = null;
+							handler.onAuthenticationFailure(request, response, exception);
+						})
+				);
 		// @formatter:on
 	}
 
